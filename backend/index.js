44cff21850a2b7d9d1d1bbd8a3c05569b6b155a0
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2025-02-25 16:08:36 +0530

    Added a 10s Delay in Response

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.use('/' , ((req,res)=>{
-    res.send("Hello World!")
-}))
+app.use('/', async (req, res) => {
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    res.send('Hello World!');
+  });
 
 app.listen(8001 , (()=>{
     console.log("App Started")
